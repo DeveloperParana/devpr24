2fa15114c96e3664690b006c77b436c76bc04d50
Ivo <byivo@byivo.local>
2024-06-03 23:55:36 -0300

    remove everything done previosuly

--- src/utils/imagesloaded.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'imagesloaded' {
-    function imagesLoaded(element: Element | NodeList | Array<Element> | string): ImagesLoaded;
-
-    interface ImagesLoaded {
-        on(event: string, listener: Function): void;
-    }
-
-    export = imagesLoaded;
-}
